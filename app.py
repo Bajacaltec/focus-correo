6ca738bd085c2b5dc92c156709daab6d9f0862fc
mdfnunez <65826102+mdfnunez@users.noreply.github.com>
2023-03-31 14:15:25 -0600

    Create app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+from streamlit_lottie import st_lottie
+import requests
+
+
+st.title('Gracias por ponerte en contacto con nosotros')
+st.subheader('En breve recibiras un correo electrónico de nuestra parte')
+
+def load_lottieurl(url: str):
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None
+    return r.json()
+gracias=load_lottieurl('https://assets2.lottiefiles.com/packages/lf20_vzy3decs.json')
+st_lottie(gracias,height=300)
